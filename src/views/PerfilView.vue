fd14f283a47e36a4ac8d6829f04f85a381797a4d
ren4yi <hannajuliasch@gmail.com>
2026-08-11 16:31:14 -0300

    FEAT - Criação do Template

---
+++ src/views/PerfilView.vue
@@ -0,0 +1,104 @@
+<script setup>
+
+</script>
+
+<template>
+<section>
+<h2>Kennedy Araújo</h2>
+<ul>
+    <li>
+        CPF: 011.201.321-45
+    </li>
+    <li>
+        CEP: 67.800.912
+    </li>
+    <li>
+        Telefone: +55 47 9 5467-9814
+    </li>
+    <li>
+        Tipo Sanguineo: A+
+    </li>
+    <li>
+        Email: kennedyA@gmail.com
+    </li>
+</ul>
+</section>
+
+<section>
+<h3>
+    Sua consulta é hoje!
+</h3>
+<p>
+ Consulta marcada
+</p>
+ <ul>
+    <li>
+        Tipo: Exame de Sangue
+    </li>
+    <li>
+        Hora: 10:30h
+    </li>
+    <li>
+        Dr. Fábio Longo de Moura
+    </li>
+    <li>
+        Local: Hospital São Bernadinho
+    </li>
+ </ul>
+</section>
+
+<section>
+    <ul>
+        Histórico de Vacinas
+        <li>
+            Dengue
+            <ul>
+                <li>
+                    Dia: 17 / 03 / 26
+                </li>
+                <li>
+                    Horário: 11:05
+                </li>
+                <li>
+                    Local: Hospital São Bernadino
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            Covid-19
+            <ul>
+                <li>
+                    Dia: 03 / 02 / 26
+                </li>
+                <li>
+                    Horário: 08:45
+                </li>
+                <li>
+                    Local: Hospital São José
+                </li>
+            </ul>
+        </li>
+
+        <li>
+            Gripe
+            <ul>
+                <li>
+                    Dia: 24 / 02 / 26
+                </li>
+                <li>
+                    Horário: 09:05
+                </li>
+                <li>
+                    Local: Hospital Santa Helena
+                </li>
+            </ul>
+        </li>
+    </ul>
+</section>
+
+</template>
+
+<style scoped>
+
+</style>
