48af81ee1a7614002b890b3f8f9fcbf6141d0ff8
tiago-bah <cardoso.tiago1907@gmail.com>
2026-08-12 16:36:06 -0300

    Faet---SobreNos: estrutura montada

---
+++ src/views/SobreNosView.vue
@@ -0,0 +1,92 @@
+<template>
+  <main>
+    <section class="sobreProj">
+      <header>
+        <h1>Sobre Nós</h1>
+        <h2>Sobre o Projeto</h2>
+      </header>
+      <p>O Instituto Med é um site criado para facilitar o acesso a saúde publica e privada. Aqui você tem acesso a
+         informação e agendamento de atendimentos sem sair de casa!</p>
+    </section>
+
+    <section class="eqipe">
+      <h2>Sobre a Equipe</h2>
+      <p>Somos um grupo de estudantes do segundo ano do ensino médio formado para a criação e desenvolvimento de um
+         projeto escolar do Instituto Federal Catarinense (IFC).</p>
+    </section>
+
+    <section class="dev">
+      <h2>Desenvolvedores</h2>
+      <ul>
+        <li>
+          <article>
+            <h3>Ana Júlia Bonfim Moraes</h3>
+            <span>Designer</span>
+            <p>Responsável na criação do design gráfico do site e de sua identidade visual, aliar criatividade,
+               funcionalidade e estética.</p>
+          </article>
+        </li>
+
+        <li>
+          <article>
+            <h3>Giovana Letícia Bandoch</h3>
+            <span>Designer</span>
+            <p>Responsável na criação do design gráfico do site e de sua identidade visual, em aliar criatividade,
+               funcionalidade e estética.</p>
+          </article>
+        </li>
+
+        <li>
+          <article>
+            <h3>Hanna Julia Scheroeder</h3>
+            <span>Adinistrador</span>
+            <p>Responsável na criação do modelo lógico do banco de dados, por instalar, configurar, gerenciar e
+               otimizar banco de dados.</p>
+          </article>
+        </li>
+
+        <li>
+          <article>
+            <h3>Lucas Pereira de Oliveira</h3>
+            <span>Análista</span>
+            <p>Responsável na criação dos requisitos, em traduzir as necessidades dos usuários de software e do
+               sistema.</p>
+          </article>
+        </li>
+
+        <li>
+          <article>
+            <h3>Nicholas Oyafuso</h3>
+            <span>Pesquisador</span>
+            <p>Responsável na pesquisa e segurança da informação, garantindo a vericidade das noticias.</p>
+          </article>
+        </li>
+
+        <li>
+          <article>
+            <h3>Tiago Cardoso</h3>
+            <span>Projetista</span>
+            <p>Responsável na criação do modelo conceitual do banco de dados no brModelo.</p>
+          </article>
+        </li>
+      </ul>
+    </section>
+
+    <footer class="contato">
+      <img src="" alt="">
+      <h2>Contato</h2>
+      <address>
+        <div class="carti">
+          <img src="" alt="">
+          <a href="#">Email: institutoMed@gmail.com</a>
+        </div>
+        <div class="tell">
+          <img src="" alt="">
+          <p></p>
+        </div>
+      </address>
+    </footer>
+  </main>
+</template>
+
+<style scoped></style>
