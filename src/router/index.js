db664a8481ba005617afa1fe0e2f84d60d6b8059
tiago-bah <cardoso.tiago1907@gmail.com>
2026-08-16 21:22:44 -0300

    Feat---SobreNos: Finalizada a Página Sobre Nós

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import SobreNosView from '@/views/SobreNosView.vue'
+import SobreNosView from '@/views/SobrenosView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
