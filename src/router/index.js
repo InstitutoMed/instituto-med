a2c5f358de93a7cf5f4855216b1085304b050091
anajuliamalfim <anajuliabonfim129@gmail.com>
2026-08-18 14:25:17 -0300

    FEAT - Caderneta: atualização

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import cadernetaView from '@/views/cadernetaView.vue'
-import homeView from '@/views/homeView.vue'
 import AgendarView from '@/views/agendarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/home',
-      name: 'home',
-      component: homeView,
-    },
+ 
     {
       path: '/caderneta',
       component: cadernetaView,
