e428a9a2c4901d2aa67ac1fd874c03aed9be30be
ren4yi <hannajuliasch@gmail.com>
2026-08-23 23:58:22 -0300

    FEAT - Alteração nas rotas [meta]

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,29 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: { title: 'Instituto Med — Sua saúde conectada' }
+  },
+    {
       path: '/profile', 
       name: 'profile',
       component: () => import('../views/ProfileView.vue'),
+        meta: { title: 'Minha conta — Instituto Med' }
     },
     {
       path: '/fluxograma',
       name: 'fluxograma',
       component: () => import('../views/FluxogramaView.vue'),
+         meta: { title: 'Fluxograma Hospitais — Instituto Med' }
+     
     },
     {
       path: '/editprofile',
       name: 'editprofile',
       component: () => import('../views/EditProfileView.vue'),
+         meta: { title: 'Editar conta — Instituto Med' }
     }
   ],
 })
